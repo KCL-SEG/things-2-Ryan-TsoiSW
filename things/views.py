224ee7c11ca828125c8e8b7d25badf1d79bc5e3c
Ryan Tsoi <sanwingtsoi@gmail.com>
2022-12-08 07:33:08 +0000

    ex2

--- things/views.py
+++ things/views.py
@@ -1,6 +1,7 @@
+
 from django.shortcuts import render
-from .models import Thing
 from .forms import ThingForm
+
 def home(request):
     form = ThingForm()
     return render(request, 'home.html', {'form': form})
